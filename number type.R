d3152fa8053956e9f18962295480bb8ce4edbfeb
Mujahid Raj <113965511+mujahidraj@users.noreply.github.com>
2025-03-22 14:09:03 +0600

    Add files via upload

---
+++ number type.R
@@ -0,0 +1,21 @@
+numeric <- 100 #this is a numeric number
+class(numeric)
+
+integer <- 50L #this is a integer number
+class(integer)
+
+complex <- 9+3i #this is a complex number
+class(complex)
+
+character <- "Hello world"
+class(character)
+
+logical <- TRUE
+class(logical)
+
+
+numeric
+integer
+complex
+character
+logical
